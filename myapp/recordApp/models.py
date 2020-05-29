268df72c0a2db0b8fbd74e9061e08b6b6baa6070
Kunal Dulani <kunaldulani@icloud.com>
2020-05-29 17:30:08 -0600

    Added Database functionality - allApplications page reads from Applications to display all Recods. - index page message changed. - used migrations functionality to create and modify sqllite database

--- myapp/recordApp/models.py
+++ myapp/recordApp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+# Application record
+class Application(models.Model):
+    organizationName = models.CharField(max_length=120)
+    status = models.CharField(max_length=50)
+    comments = models.CharField(max_length=300)
+    link = models.CharField(max_length=500)
+    dateApplied = models.DateField(default=timezone.now)
+
+    def __str__(self):
+        return f"Org: {self.organizationName}, Status: {self.status} On {self.dateApplied}"
--- myapp/recordApp/views.py
+++ myapp/recordApp/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Application
 
 # My vies/routes
 
+# Home Page
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "recordApp/index.html")
+
+# Display all applications
+def allApplications(request):
+    context = {
+        "applications": Application.objects.all()
+    }
+    return render(request, "recordApp/allApplications.html", context)
